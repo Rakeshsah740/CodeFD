d26b5b7893e5df27853e02fbcdb55a1b1d9f9ab9
rakesh127194 <169039652+Rakeshsah740@users.noreply.github.com>
2025-07-18 14:38:41 +0200

    Update error.py

--- error.py
+++ error.py
@@ -1,7 +1,7 @@
 
 import numpy as np
 
-def compute_error(ux, uy, ux_mid_prev, uy_mid_prev, Ny, Nx, eps=1e-12):
+def compute_error(ux, uy, ux_prev, uy_prev, Ny, Nx, eps=1e-12):
     """
     Compute maximum relative error in velocity at center lines.
     """
@@ -9,12 +9,11 @@ def compute_error(ux, uy, ux_mid_prev, uy_mid_prev, Ny, Nx, eps=1e-12):
     uy_mid = uy[:, Nx // 2]
 
     # Avoid division by zero by adding eps
-    error_ux_rel = np.max(np.abs(ux_mid - ux_mid_prev) / (np.abs(ux_mid_prev) + eps))
-    error_uy_rel = np.max(np.abs(uy_mid - uy_mid_prev) / (np.abs(uy_mid_prev) + eps))
+    error_ux_rel = np.max(np.abs(ux_mid - ux_prev) / (np.abs(ux_prev) + eps))
+    error_uy_rel = np.max(np.abs(uy_mid - uy_prev) / (np.abs(uy_prev) + eps))
     total_error_rel = max(error_ux_rel, error_uy_rel)
     error_ux_abs = np.max(np.abs(ux_mid - ux_prev))
     error_uy_abs = np.max(np.abs(uy_mid - uy_prev))
     total_error_abs = max(error_ux_abs, error_uy_abs)
 
-    return total_error_abs, total_error_rel, ux__mid_prev.copy(), uy__mid_prev.copy()
-
+    return total_error_abs, total_error_rel, ux_mid.copy(), uy_mid.copy()
